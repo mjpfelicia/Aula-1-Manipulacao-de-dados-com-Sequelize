3b56d7cdb930995b98a0dd6b3cedbc659f1572cb
Sandra Felicio <mjpfelicia@gmail.com>
2024-03-03 14:51:13 -0300

    criando banco de  dados da empresa

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-
-
 // Importando as biliotecas
-const{ Sequelize, Model, DataTypes} = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
 //Abrindo conexão com o Banco de dados ou criando um novo caso não exista
 const sequelize = new Sequelize({
   dialect: "sqlite",
-storage: "empresa.sqlite"
-  });
+  storage: "empresa.sqlite"
+});
 
+class Setor extends Model {
+
+  static init(sequelize) {
+    super.init({
+      idsetor: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      nome: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+      },
+      ramal: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+      }
+    }, { sequelize, modelname: 'setor', tableName: 'setores' })
+  }
+}
+
+Setor.init(sequelize);
 
 (async () => {
-  await sequelize.sync({force: true});
+  const { modelManager } = await sequelize.sync({ force: true });
+
+  console.table({ tabelas: modelManager.all })
+
 })();
